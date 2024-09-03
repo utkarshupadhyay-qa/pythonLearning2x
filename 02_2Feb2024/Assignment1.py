670b7e9d269b08d0a12974002da34238c2752389
utkarshupadhyay11 <rameshedlogics001@gmail.com>
2024-09-03 13:12:43 +0530

    [Utkarsh] Add Assignment-1.py

--- 02_2Feb2024/Assignment1.py
+++ 02_2Feb2024/Assignment1.py
@@ -2,4 +2,27 @@
 
 # Task 2 - Print the table of 2 by using the command print()
 
+# Task 1
 
+name = input("Enter your name : ")
+age = input("Please Enter your age : ")
+roll_number = input("Please enter yuor roll number: ")
+phone_number = input("Please Enter you phone number: ")
+print("User name is: ", name)
+print("User age is: ", age)
+print("User roll number is: ", roll_number)
+print("User phone number is: ", phone_number)
+
+# Task 2
+print("Table of 2 is as folows: ")
+print("---------")
+print("2*1= ",2*1)
+print("2*2= ",2*2)
+print("2*3= ",2*3)
+print("2*4= ",2*4)
+print("2*5= ",2*5)
+print("2*6= ",2*6)
+print("2*7= ",2*7)
+print("2*8= ",2*8)
+print("2*9= ",2*9)
+print("2*10= ",2*10)
