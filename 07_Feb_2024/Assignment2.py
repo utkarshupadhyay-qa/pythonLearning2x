b421ed8e7c135a3ea514fca0071cb85f82cff7bf
UTKARSH <utkarsh.upadhyay.qa@gmail.com>
2025-02-02 00:35:09 +0530

    hi

--- 07_Feb_2024/Assignment2.py
+++ 07_Feb_2024/Assignment2.py
@@ -10,3 +10,22 @@
 # Develop a Python script that calculates the square and cube of a given number.
 
 # Task 1
+
+radius1 = int(input("Enter the radius of the circle: "))
+pi = 3.14
+area1 = pi*radius1**2
+print("The are of the circle is: ",area1)
+
+# Task2
+
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
+print("Is first number greater than second number :{} ".format(first_number > second_number))
+print("Is first number less than second number : ",first_number < second_number)
+print("Is first number equal to second number :{} ".format(first_number == second_number))
+
+#Task 3
+
+number1 = int(input("Enter the given number for whom we want to calculate the square and cube: "))
+print("The square of the number {} is {} ".format(number1,number1**2))
+print("The cube of the number {} is {} ".format(number1,number1**3))
