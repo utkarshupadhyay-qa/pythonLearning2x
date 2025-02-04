bb15d10d70f76f2e637fbb20d4e6bd6c11064753
UTKARSH <utkarsh.upadhyay.qa@gmail.com>
2025-02-04 15:51:08 +0530

    hi

--- 07_Feb_2024/Assignment2.py
+++ 07_Feb_2024/Assignment2.py
@@ -11,7 +11,7 @@
 
 # Task 1
 
-radius1 = int(input("Enter the radius of the circle: "))
+radius1 = float(input("Enter the radius of the circle: "))
 pi = 3.14
 area1 = pi*radius1**2
 print("The are of the circle is: ",area1)
