a1ec1fb79da22b165db84b4650c24c4d4002fd54
UTKARSH <utkarsh.upadhyay.qa@gmail.com>
2025-05-23 23:51:39 +0530

    hi

--- 12_Feb_2024/Lab031.py
+++ 12_Feb_2024/Lab031.py
@@ -1,5 +1,5 @@
 # Break
-# For -> 1 ti 10 -> range(1,11,1) , range(1,11)
+# For -> 1 to 10 -> range(1,11,1) , range(1,11)
 #i =5 -> break out from loop -> kicked out from the loop
 
 for counter in range(1,11):
